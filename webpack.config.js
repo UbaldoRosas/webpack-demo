6b5a30199760797289ce73484b7757f573196c5f
UbaldoRosas <liadz.orb@gmail.com>
2019-10-25 13:48:07 -0500

    output management config

--- webpack.config.js
+++ webpack.config.js
@@ -1,54 +1,21 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const {CleanWebpackPlugin} = require('clean-webpack-plugin');
 
 module.exports = {
 	mode: "production",
-    entry: './src/index.js',
+    entry: {
+		app : './src/index.js',
+		print : './src/print.js'
+	},
+	plugins : [
+		new CleanWebpackPlugin(),
+		new HtmlWebpackPlugin({
+			title : 'Output Management',
+		})
+	],
     output : {
-        filename : 'bundle.js',
+        filename : '[name].bundle.js',
         path : path.resolve(__dirname, 'dist')
     },
-    module : {
-        rules : [
-            {
-                test : /\.css$/,
-                use : [
-                    'style-loader',
-                    'css-loader'
-                ]
-
-            },
-            {
-                test : /\.(png|svg|jpg|gif)$/,
-                use : [
-                    'file-loader',
-                    {
-                        loader : 'image-webpack-loader',
-                        options : {
-                            bypassOnDebug : true,
-                            disable : true,
-                            optipng: {
-                                enabled: false,
-                            },
-                            pngquant: {
-                                quality: [0.65, 0.90],
-                                speed: 4
-                            },
-                        }
-                    }
-                ]
-            },
-            {
-                test: /\.(woff|woff2|eot|ttf|otf)$/,
-                use: [
-                    'file-loader',
-                ],
-            },
-            {
-                test : /\.xml$/,
-                use : [
-                    'xml-loader'
-                ]
-            }
-        ]
-    }
 }
