42676432115d24d813c7a02afe4489b871d8c3aa
UbaldoRosas <liadz.orb@gmail.com>
2019-10-25 15:15:41 -0500

    Webpack watch config

--- webpack.config.js
+++ webpack.config.js
@@ -1,17 +1,18 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
-const {CleanWebpackPlugin} = require('clean-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
 module.exports = {
-	mode: "production",
+	mode: "development",
     entry: {
 		app : './src/index.js',
 		print : './src/print.js'
 	},
+	devtool: 'inline-source-map',
 	plugins : [
 		new CleanWebpackPlugin(),
 		new HtmlWebpackPlugin({
-			title : 'Output Management',
+			title : 'Output',
 		})
 	],
     output : {
